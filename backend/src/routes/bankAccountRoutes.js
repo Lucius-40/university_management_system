8af913ed7aec515665faa1abdf5b232c44c1a70f
Lucius-Viper <tanjinulislam16@gmail.com>
2026-01-09 11:29:20 +0600

    added authenticate token to bank routes

--- backend/src/routes/bankAccountRoutes.js
+++ backend/src/routes/bankAccountRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const BankAccountController = require('../controllers/bankAccountController.js');
+const AuthenticateToken = require('../middlewares/authenticateToken.js');
+
+const auth = new AuthenticateToken();
 
 const bankAccountRouter = express.Router();
 const controller = new BankAccountController();
 
-bankAccountRouter.post('/create-account', controller.create);
-bankAccountRouter.patch('/update-account', controller.update );
-bankAccountRouter.get('/get-account/:id', controller.getById);
-bankAccountRouter.delete('/delete-account/:id', controller.delete);
+bankAccountRouter.post('/create-account',auth.authenticateToken, controller.create);
+bankAccountRouter.patch('/update-account',auth.authenticateToken ,controller.update );
+bankAccountRouter.get('/get-account/:id', auth.authenticateToken,controller.getById);
+bankAccountRouter.delete('/delete-account/:id',auth.authenticateToken ,controller.delete);
 
 module.exports = bankAccountRouter ;
 
