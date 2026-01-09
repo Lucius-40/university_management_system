4b15d9a51c366d9f7f04ee775cfaae2962552796
unknown <pritombiswas9999@gmail.com>
2026-01-09 19:09:33 +0600

    fixed issues with dotenv position and path

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,14 @@ const dotenv = require('dotenv');
 const path = require('path');
 const cors = require('cors');
 
+dotenv.config({path: path.resolve(__dirname, './.env')});
+// console.log(process.env.DATABASE_URL);
+
+
 const authRouter = require('./routes/authRoutes.js')
 const userRouter = require('./routes/userRoutes.js')
 const bankAccountRouter = require('./routes/bankAccountRoutes.js')
 
-dotenv.config({path: path.resolve(__dirname, '../.env')});
 const app = express();
 app.use(express.json());
 app.use(cors());
