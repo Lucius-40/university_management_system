93d0431da9b5cafffa928340158fc2ddb5ef0c96
Lucius-Viper <tanjinulislam16@gmail.com>
2026-02-26 10:50:53 +0600

    poodle

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const cors = require('cors');
+const tableModel = require('./models/tableModels');
 
 dotenv.config({path: path.resolve(__dirname, './.env')});
 
@@ -9,17 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//     res.send('University backend running.');
-// });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
 });
 
-// Test DB connection after server starts
-const tableModel = require('./models/tableModels');
+
+
 (async () => {
     try {
         const res = await tableModel.testConnection();
